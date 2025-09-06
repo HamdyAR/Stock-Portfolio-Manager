78473f4cd4cd284107a8c55a11b2904581b1b332
HamdyAR <aremu.hamdalah@gmail.com>
2025-09-06 12:09:38 +0100

    feat: successful Spring Boot application setup - Database connection to MySQL established - Application runs on port 8080 - Hibernate amd JPA configured properly - Ready for entity development
    Closes #1

---
+++ src/test/java/com/cbfacademy/stockportfoliomanager/StockPortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.stockportfoliomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
